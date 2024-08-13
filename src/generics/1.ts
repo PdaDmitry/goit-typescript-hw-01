fa981fe6ccf157b3e628d160053d0b5c8aca910a
PdaDmitry <146446934+PdaDmitry@users.noreply.github.com>
2024-08-13 19:32:01 +0300

    generics/1.ts

---
+++ src/generics/1.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+async function fetchData(url: string) {
+  try {
+    const response: unknown = await axios.get(url);
+    if (typeof response === "object" && response !== null) {
+      return response.data;
+    }
+  } catch (error) {
+    throw new Error(`Error fetching from ${url}: ${error}`);
+  }
+}
---
+++ src/generics/3.ts
@@ -0,0 +1,11 @@
+// function merge(objA, objB) {
+//   return Object.assign(objA, objB);
+// }
+
+// function merge<T, U>(objA: T, objB: U) {
+//   return Object.assign(objA, objB);
+// }
+
+// const merged = merge({ name: "Alisa" }, { age: 28 });
+
+// merged.name;
