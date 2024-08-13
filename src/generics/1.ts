21f8ad06c53b438f0828ddb00093ebe75f473bc2
PdaDmitry <146446934+PdaDmitry@users.noreply.github.com>
2024-08-13 19:41:46 +0300

    fetchData

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -4,9 +4,11 @@ async function fetchData(url: string) {
   try {
     const response: unknown = await axios.get(url);
     if (typeof response === "object" && response !== null) {
-      return response.data;
+      return response; //return response.data;
     }
   } catch (error) {
     throw new Error(`Error fetching from ${url}: ${error}`);
   }
 }
+
+fetchData();
